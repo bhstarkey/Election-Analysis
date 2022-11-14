9a9df2af0cdb0f76d755cbed175516f769688c23
bhstarkey <116474586+bhstarkey@users.noreply.github.com>
2022-11-13 20:24:53 -0700

    updated python file for 3.2.10

---
+++ Python_Practice.py
@@ -0,0 +1,35 @@
+#counties = ["Arapahoe","Denver","Jefferson"]
+#if counties[3] != 'Jefferson':
+#    print(counties[2])
+#
+#What is the temperature?
+#temperature = int(input("What is the temperature outside?"))
+#
+#if temperature > 80:
+#    print("Turn on the AC.")
+#else:
+#    print("Open the windows.")
+
+#What is the score?
+#score = int(input("What is your test score?"))
+
+#Determine the grade.
+#if score >= 90:
+#    print('Your grade is an A.')
+#elif score >= 80:
+#    print('Your grade is a B.')
+#elif score >=70:
+#    print('Your grade is a C.')
+#elif score >= 60:
+#    print('Your grade is a D.')
+#else:
+#    print('Your grade is an F')
+
+counties = ["Arapahoe","Denver","Jefferson"]
+#if "Arapahoe" in counties and "El Paso" not in counties:
+#    print("Only Arapahoe is in the list of counties.")
+#else:
+#    print("Arapahoe  is in the list of counties ad El Paso is not in the list of counties")
+
+for i in range(len(counties)):
+    print(counties[i])

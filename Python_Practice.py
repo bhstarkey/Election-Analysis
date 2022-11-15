0d25c431ccacfc58b9671da975491ee511bb1783
bhstarkey <116474586+bhstarkey@users.noreply.github.com>
2022-11-14 18:31:20 -0700

    updated python code

--- Python_Practice.py
+++ Python_Practice.py
@@ -25,11 +25,49 @@
 #else:
 #    print('Your grade is an F')
 
-counties = ["Arapahoe","Denver","Jefferson"]
+#counties = ["Arapahoe","Denver","Jefferson"]
 #if "Arapahoe" in counties and "El Paso" not in counties:
 #    print("Only Arapahoe is in the list of counties.")
 #else:
 #    print("Arapahoe  is in the list of counties ad El Paso is not in the list of counties")
 
-for i in range(len(counties)):
-    print(counties[i])
+#for i in range(len(counties)):
+#    print(counties[i])
+
+#counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+#for county, voters in counties_dict.items():
+#    print(f"{county} county has {voters:,} registered voters.")
+
+
+#fstring printing
+#my_votes = int(input("How many votes did you get in the election? "))
+#total_votes = int(input("What is the total votes in the election? "))
+#percentage_votes = (my_votes / total_votes) * 100
+#print(f"I received {my_votes / total_votes * 100}% of the total votes")
+
+#fstring dictionaries
+#counties_dict = {"Arapahoe": 369237, "Denver":413229, "Jefferson": 390222}
+#for county, voters in counties_dict.items():
+#    print(f"{county} county has {voters} registered voters")
+
+#Multi line Fstrings
+#candidate_votes = int(input("How many votes did you get in the election? "))
+#total_votes = int(input("What is the total votes in the election? "))
+#message_to_candidate =(
+#    f"You received {candidate_votes:,} number of votes."
+#    f"The total number of votes in the election was {total_votes:,}."
+#    f"You received {candidate_votes / total_votes * 100:.2f}% of the total votes.")
+
+#print(message_to_candidate)
+
+voting_data = [``{"county":"Arapahoe", "registered_voters": 422829}, {"county":"Denver", "registered_voters": 463353}, {"county":"Jefferson", "registered_voters": 432438}]
+    for county_dict, registered_voters_dict in voting_data
+    print(f"{county_dict['county']} county has {registered_voters_dict['voters']:,} registered voters")
+
+
+#voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+#                {"county":"Denver", "registered_voters": 463353},
+#                {"county":"Jefferson", "registered_voters": 432438}]
+
+#for county_dict in voting_data:
+#    print(county_dict["county"])
